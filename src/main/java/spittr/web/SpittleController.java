34866baa8de1edf01274881b960d0ff3ca117977
KWONS <KWONS@JFSLT392660.msds.jfs.ohio.gov>
2016-06-14 15:39:08 -0400

    update

--- src/main/java/spittr/web/SpittleController.java
+++ src/main/java/spittr/web/SpittleController.java
@@ -1,17 +1,22 @@
 package spittr.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import spittr.Spittle;
 import spittr.data.SpittleRepository;
 
 @Controller
 @RequestMapping("/spittles")
 public class SpittleController {
     private SpittleRepository spittleRepository;
+    private static final String MAX_LONG_AS_STRING = Long.toString(Long.MAX_VALUE);
 
     @Autowired
     public SpittleController(SpittleRepository spittleRepository) {
@@ -19,8 +24,9 @@ public class SpittleController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public String spittles(Model model) {
-	model.addAttribute("spittleList", spittleRepository.findSpittles(Long.MAX_VALUE, 20));
-	return "spittles";
+    public List<Spittle> spittles(
+	@RequestParam(value = "max", defaultValue = MAX_LONG_AS_STRING) long max,
+	@RequestParam(value = "count", defaultValue = "20") int count) {
+	return spittleRepository.findSpittles(max, count);
     }
 }
