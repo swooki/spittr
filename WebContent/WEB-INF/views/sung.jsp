0db7ac7e1f5143100235205cc0a06fee8d203116
KWONS <KWONS@JFSLT392660.msds.jfs.ohio.gov>
2016-06-14 10:57:15 -0400

    Adds a sample page for /Sung

--- WebContent/WEB-INF/views/sung.jsp
+++ WebContent/WEB-INF/views/sung.jsp
@@ -2,14 +2,12 @@
 <%@ page session="false"%>
 <html>
 <head>
-<title>Home</title>
+<title>Sung</title>
 <link rel="stylesheet" type="text/css"
 	href="<c:url value="/resources/style.css"/>">
 </head>
 <body>
-	<h1>Home</h1>
-	<a href="<c:url value="/spittles" />">Spittles</a> |
-	<a href="<c:url value="/spitter/register"/>">Register</a> |
-	<a href="<c:url value="/Sung"/>">Sung</a>
+	<h1>Sung</h1>
+	<a href="<c:url value="/" />">Home</a>
 </body>
 </html>
--- src/test/java/spittr/web/SungControllerTest.java
+++ src/test/java/spittr/web/SungControllerTest.java
@@ -8,11 +8,11 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-public class HomeControllerTest {
+public class SungControllerTest {
     @Test
-    public void testHomePage() throws Exception {
-	HomeController controller = new HomeController();
+    public void testSungPage() throws Exception {
+	SungController controller = new SungController();
 	MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-	mockMvc.perform(MockMvcRequestBuilders.get("/")).andExpect(MockMvcResultMatchers.view().name("home"));
+	mockMvc.perform(MockMvcRequestBuilders.get("/Sung")).andExpect(MockMvcResultMatchers.view().name("sung"));
     }
 }
